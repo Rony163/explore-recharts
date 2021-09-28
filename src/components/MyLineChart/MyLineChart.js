4e687052cbaed75dc0a6e2bce90c95fba2342a5d
Rony163 <minhajulislamrony43@gmail.com>
2021-09-28 20:50:14 +0600

    error fixes

--- src/components/MyLineChart/MyLineChart.js
+++ src/components/MyLineChart/MyLineChart.js
@@ -1,70 +1,75 @@
 import React from 'react';
-import { CartesianGrid, Line, LineChart, ResponsiveContainer, XAxis, YAxis } from 'recharts';
+import { CartesianGrid, Legend, Line, LineChart, Tooltip, XAxis, YAxis } from 'recharts';
 
 const MyLineChart = () => {
     const data = [
         {
-            name: 'Page A',
-            uv: 4000,
+            name: "Company A",
+            mobile: 4000,
             pv: 2400,
-            amt: 2400,
+            amt: 2400
         },
         {
-            name: 'Page B',
-            uv: 3000,
+            name: "Company B",
+            mobile: 3000,
             pv: 1398,
-            amt: 2210,
+            amt: 2210
         },
         {
-            name: 'Page C',
-            uv: 2000,
+            name: "Company C",
+            mobile: 2000,
             pv: 9800,
-            amt: 2290,
+            amt: 2290
         },
         {
-            name: 'Page D',
-            uv: 2780,
+            name: "Company D",
+            mobile: 2780,
             pv: 3908,
-            amt: 2000,
+            amt: 2000
         },
         {
-            name: 'Page E',
-            uv: 1890,
+            name: "Company E",
+            mobile: 1890,
             pv: 4800,
-            amt: 2181,
+            amt: 2181
         },
         {
-            name: 'Page F',
-            uv: 2390,
+            name: "Company F",
+            mobile: 2390,
             pv: 3800,
-            amt: 2500,
+            amt: 2500
         },
         {
-            name: 'Page G',
-            uv: 3490,
+            name: "Company G",
+            mobile: 3490,
             pv: 4300,
-            amt: 2100,
-        },
+            amt: 2100
+        }
     ];
     return (
-        <ResponsiveContainer width="100%" height="100%">
-            <LineChart
-                width={500}
-                height={300}
-                data={data}
-                margin={{
-                    top: 5,
-                    right: 30,
-                    left: 20,
-                    bottom: 5,
-                }}
-            >
-                <CartesianGrid strokeDasharray="3 3" />
-                <XAxis dataKey="name" />
-                <YAxis />
-                <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
-            </LineChart>
-        </ResponsiveContainer>
+        <LineChart
+            width={800}
+            height={300}
+            data={data}
+            margin={{
+                top: 5,
+                right: 30,
+                left: 20,
+                bottom: 5
+            }}
+        >
+
+            <XAxis dataKey="name" />
+            <YAxis />
+
+            <Line
+                type="monotone"
+                dataKey="pv"
+                stroke="#8884d8"
+                activeDot={{ r: 8 }}
+            />
+            <Line type="monotone" dataKey="mobile" stroke="#82ca9d" />
+        </LineChart>
     );
 };
 
